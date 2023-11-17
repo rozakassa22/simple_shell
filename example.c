3d8066b856ab833a8d417c73a3ad55795351e33e
rozakassa22 <kassaroza22@gmail.com>
2023-11-18 00:02:09 +0300

    my commit

--- example.c
+++ example.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
 /**
- *print_hello- Prints "Hello, world!"
  * main - Entry point of the program
- *int num - Stores the value 10 
- * main - Entry point of the program
- *Return: Always 0 (Success)
- * **/
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num1 = 10;
+    int num2 = 5;
+    int sum;
 
+    sum = num1 + num2;
 
-void print_hello();
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
 
-int main() {
-    
-    int num = 10;
-    
-    
-    print_hello();
-    
-    
-    printf("The value of num is: %d\n", num);
-    
     return 0;
-}
-
-
-void print_hello() {
-    printf("Hello, world!\n");
 }
